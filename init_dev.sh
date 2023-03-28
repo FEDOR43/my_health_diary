c428d8ac27cd9801b5f276ddc216a718a6486bb3
Fedorov Anton <afedorov43@gmail.com>
2023-03-28 21:47:59 +0300

    add init script

---
+++ init_dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# инициализация стартовых настроек при развёртывании нового проекта.
+# перед запуском скрипта установить права на выполнение. chmod ugo+x init_dev.sh
+# запуск: ./init_dev.sh
+
+sudo chmod +x ./logrotate.sh
+sudo chmod +x ./manage.py
+sudo ./logrotate.sh
+sudo ln -s /home/web/my_health_diary/conf.d/nginx/mhd_dev.conf /etc/nginx/conf.d/
+sudo nginx -s reload
+
+sudo docker-compose up -d --build
+sudo docker-compose exec app python manage.py migrate
+sudo docker-compose exec app python manage.py collectstatic
+#sudo docker-compose exec app python manage.py database_init
